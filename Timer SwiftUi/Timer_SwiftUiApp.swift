b4bcaae5dcdf565f2bef0d6e5081a690a6916b80
Bulat Sagdiev <com.BulatSagdiev>
2021-12-11 20:48:57 +0300

    Initial Commit

---
+++ Timer SwiftUi/Timer_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Timer_SwiftUiApp.swift
+//  Timer SwiftUi
+//
+//  Created by Булат Сагдиев on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Timer_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
